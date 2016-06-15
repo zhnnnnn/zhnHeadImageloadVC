fe8dcc71b196a8e2c7eace265bf45ebc485512d5
zhnnnnn <475816908@qq.com>
2016-06-15 11:29:02 +0800

    以前那个有点bug删了，重新传一份

--- zhnHeadImageloadVC/ViewController.m
+++ zhnHeadImageloadVC/ViewController.m
@@ -7,16 +7,33 @@
 //
 
 #import "ViewController.h"
+#import "ZHNheadImageLoadViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic,weak) UIImageView * tempImage;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    UIImageView * tempImage = [[UIImageView alloc]init];
+    [self.view addSubview:tempImage];
+    tempImage.frame = CGRectMake(0, 100, 100, 100);
+    self.tempImage = tempImage;
+}
+- (IBAction)clickPresent:(id)sender {
+    
+    ZHNheadImageLoadViewController * headVC = [[ZHNheadImageLoadViewController alloc]init];
+    headVC.oldImage = [UIImage imageNamed:@"meinv"];
+    headVC.ZHNheadImageBlock = ^(UIImage * headimage){
+        self.tempImage.image = headimage;
+    };
+    [self presentViewController:headVC animated:YES completion:nil];
+    //    [self.navigationController pushViewController:headVC animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
