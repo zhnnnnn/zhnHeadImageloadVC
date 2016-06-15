b3b40398ac34c2f9ade07bf0d3111a69e65a3528
zhnnnnn <475816908@qq.com>
2016-06-15 11:25:11 +0800

    Initial Commit

---
+++ zhnHeadImageloadVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhnHeadImageloadVC
+//
+//  Created by zhn on 16/6/15.
+//  Copyright © 2016年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
